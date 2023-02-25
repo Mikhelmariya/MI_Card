221de72b76c25db332fc899ff4004a71b89130c2
Mikhelmariya <mikhela65@gmail.com>
2023-02-25 15:30:12 +0530

    mi card completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MiCard extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 40,
@@ -22,9 +23,56 @@ class MiCard extends StatelessWidget {
             Text(
               "Mikhel V Kuttickal",
               style: TextStyle(
+                  fontFamily: "Pacifico",
                   fontSize: 30,
                   color: Colors.white,
                   fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.w100),
+            ),
+            SizedBox(
+              height: 10,
+              width: 150,
+              child: Divider(color: Colors.teal.shade100),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.only(left: 25, right: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                  size: 30,
+                ),
+                title: Text(
+                  "+91 7736752805",
+                  style: TextStyle(color: Colors.teal.shade900),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.only(left: 25, right: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                  size: 30,
+                ),
+                title: Text(
+                  "mikhela65@gmail.com",
+                  style: TextStyle(color: Colors.teal.shade900),
+                ),
+              ),
             )
           ],
         ),
